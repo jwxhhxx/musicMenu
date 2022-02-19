811ead999d6db0fd0608d4426764b9119ed9e81e
jwxhhxx <774578034@qq.com>
2022-02-19 20:09:06 +0800

    a new file

--- music_menu_v2/src/main.js
+++ music_menu_v2/src/main.js
@@ -1,20 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import axios from 'axios'
-import md5 from 'js-md5'
-import store from './store'
-
-Vue.use(ElementUI)
-Vue.prototype.$axios = axios
-Vue.config.productionTip = false
-Vue.prototype.$md5 = md5
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
