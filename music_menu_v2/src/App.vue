2f0785e1bfac772cc112ffb6a18ba9ea3aa2038d
jwxhhxx <774578034@qq.com>
2022-02-19 21:02:19 +0800

    add header

--- music_menu_v2/src/App.vue
+++ music_menu_v2/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <Header></Header>
+      </el-header>
       <el-container>
         <el-main>Main</el-main>
         <el-aside width="20%">Aside</el-aside>
@@ -12,44 +14,22 @@
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
-
+  name: 'App',
+  components: {
+    Header
+  },
 }
 </script>
 
 <style>
-.el-header,
-.el-footer {
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
+body {
+  margin: 0;
+  padding: 0;
 }
-
-.el-aside {
-  background-color: #d3dce6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-main {
-  background-color: #e9eef3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
+#app {
+  width: 80%;
+  margin: auto;
 }
 </style>
---
+++ music_menu_v2/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <el-row>
+      <el-col :span="12">
+        <div class="page_title"><a class="iconfont icon-music"></a>
+          My Music
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="right_part">
+          <el-button size="small">登录</el-button>
+        </div>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header {
+  height: 50px;
+  line-height: 50px;
+}
+.page_title {
+  color: black;
+  font-weight: bold;
+}
+.page_title a {
+  border-radius: 50%;
+  border: black 1px solid;
+  font-size: 20px;
+  margin-left: 5px;
+}
+.right_part {
+  float: right;
+}
+</style>
